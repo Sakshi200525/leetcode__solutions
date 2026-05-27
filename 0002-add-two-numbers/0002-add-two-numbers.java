30a1ef7bc42677b08efa3f54905e4b42eabe553f
Sakshi Shinde <shindesakshi2514@gmail.com>
2026-05-27 08:55:39 -0700

    Time: 1 ms (100%), Space: 46.5 MB (56.11%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,6 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+///Program 1
+ class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null ){
+            return l2 ;
+        }else if ( l2 == null){
+            return l1 ;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode ans = dummy ;
+        ListNode ptr1 = l1 ;
+        ListNode ptr2 = l2 ;
+
+        int carry = 0 ;
+
+        while(ptr1!=null || ptr2!=null){
+            int val1 = ptr1!=null? ptr1.val : 0;
+            int val2 = ptr2!=null? ptr2.val : 0 ;
+            int sum = val1 + val2 + carry ;
+            int d = sum%10 ;
+            carry = sum/10 ;
+
+            ListNode temp = new ListNode(d);
+
+            dummy.next = temp ;
+            dummy = dummy.next ;
+
+            ptr1 = ptr1!=null ? ptr1.next : null;
+            ptr2 = ptr2!=null ? ptr2.next : null;
+        }
+
+        if(carry>0){
+             ListNode temp = new ListNode(carry);
+            dummy.next = temp ;
+            dummy = dummy.next ;
+        }
+        return ans.next;
+    }
+}
+
+///Program 2
 // class Solution {
 //     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 //         ListNode dummyNode = new ListNode(-1);
@@ -40,38 +82,41 @@
 //     }
 // }
 
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        int carry = 0;
+///Program 3
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         ListNode dummy = new ListNode(0);
+//         ListNode current = dummy;
+//         int carry = 0;
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int sum = carry;
-//othr
-            //    if(l1!=null)   l1 = l1.next;
-            //    if(l2!=null)   l2 = l2.next
+//         while (l1 != null || l2 != null || carry != 0) {
+//             int sum = carry;
+// //othr
+//             //    if(l1!=null)   l1 = l1.next;
+//             //    if(l2!=null)   l2 = l2.next
 
-            // if(carry != 0){
+//             // if(carry != 0){
 
-            // }
-//othr
+//             // }
+// //othr
 
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
+//             if (l1 != null) {
+//                 sum += l1.val;
+//                 l1 = l1.next;
+//             }
 
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
+//             if (l2 != null) {
+//                 sum += l2.val;
+//                 l2 = l2.next;
+//             }
 
-            carry = sum / 10;
-            current.next = new ListNode(sum % 10);
-            current = current.next;
-        }
+//             carry = sum / 10;
+//             current.next = new ListNode(sum % 10);
+//             current = current.next;
+//         }
 
-        return dummy.next;
-    }
-}
+//         return dummy.next;
+//     }
+// }
+
+
